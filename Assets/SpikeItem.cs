be47b5b5ece931365ab8d06178343c5ee189dddd
Aybike Zeynep Taşkın <71091930+castiellaa@users.noreply.github.com>
2022-10-13 17:27:13 +0300

    basic level editor

---
+++ Assets/SpikeItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeItem : MonoBehaviour
+{
+    public int ID;
+    private LevelEditorManager _editor;
+    void Start()
+    {
+        _editor = GameObject.FindGameObjectWithTag("LevelEditorManager").GetComponent<LevelEditorManager>();
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Destroy(this.gameObject);
+            _editor.ItemButtons[ID].quantity++;
+            _editor.ItemButtons[ID].quantityText.text = _editor.ItemButtons[ID].quantity.ToString();
+        }
+    }
+}
